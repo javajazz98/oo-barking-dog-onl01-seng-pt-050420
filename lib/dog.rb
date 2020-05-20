ca9a76998757c093e26245fdfea925208ec7e362
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-20 00:40:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,7 @@
 # Your code goes here!
-class Dog 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
-  
-def name
-  @this_dogs_name
-  end
-end
-
-elle=Dog.new
-elle.name= "Elle"
-Puts elle.name
-  
-  
-  
+elle = Dog.new
+elle.name = "elle"
+elle.name
+elle.bark
+Woof!
  
